b6ee1171fac6a72cee43322ae21a1d0974119722
Ryan Grove <ryan@wonko.com>
2010-03-28 16:50:24 -0700

    Improve cacheability and error handling.

--- jetpants.rb
+++ jetpants.rb
@@ -6,9 +6,16 @@ require 'erubis'
 require 'sinatra'
 require 'yajl'
 
+ERROR_MISSING_QUERY = 'No search query specified.'
+
 BOSSMan.application_id = 'C82ywM3V34FSPr8EUqeE61WL6zQ1psBVArWY62MoVxyuljPRIfkcdnWSKDTKBuTkFsyMAkvp9Q--'
 
 helpers do
+  def json_error(message, code = 500)
+    cache_control(:no_cache)
+    halt(code, Yajl::Encoder.encode({:error => message}))
+  end
+
   def search_web(query, options = {})
     boss = BOSSMan::Search.web(query, options)
 
@@ -39,16 +46,20 @@ helpers do
 end
 
 get '/' do
-  content_type 'text/html', :charset => 'utf-8'
+  content_type('text/html', :charset => 'utf-8')
+  cache_control(:public, :max_age => 1800)
+
   erubis :index
 end
 
 get '/api/search' do
-  content_type 'application/json', :charset => 'utf-8'
+  content_type('application/json', :charset => 'utf-8')
 
   unless query = params[:q]
-    # TODO: return 400
+    json_error(ERROR_MISSING_QUERY, 400)
   end
+
+  cache_control(:public, :max_age => 600)
 
   count = (params[:count] || 10).to_i
   start = (params[:start] || 1).to_i
